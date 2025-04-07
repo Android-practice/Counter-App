80964f49a9ddfa87eaeaa0f77998e92076515459
YJLEE <dldbwjd24@gmail.com>
2025-04-07 13:28:39 +0900

    counter app without MVVM pattern

--- app/src/main/java/com/androidpractice/counterapp/MainActivity.kt
+++ app/src/main/java/com/androidpractice/counterapp/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.androidpractice.counterapp.ui.theme.CounterAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CounterAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TheCounterApp()
                 }
             }
         }
@@ -31,17 +40,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TheCounterApp() {
+    val count = remember { mutableStateOf(0) }// rotate event가 발생하면 초기화됨 
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CounterAppTheme {
-        Greeting("Android")
+    fun increment() {
+        count.value++
+    }
+
+    fun decrement() {
+        count.value--
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = " Count : ${count.value}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row{
+            Button(onClick = {increment()}){
+                Text("+")
+            }
+            Button(onClick = {decrement()}){
+                Text("-")
+            }
+        }
     }
 }
